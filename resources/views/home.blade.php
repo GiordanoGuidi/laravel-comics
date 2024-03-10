a2deac9b8344cdbb044e455714cab5dca51574f9
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-10 19:18:26 +0100

    added section comic-details and comic-info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,23 +7,25 @@
 @section('main-content')
 {{--Main--}}
     <!--CARDS-CONTAINER-->
-    <section id="comics-list" class="small-container card-container">
-        <button class="series">CURRENT SERIES</button>
-        <button class="load">LOAD MORE</button>
-        <ul class="cards-list">
-            @foreach ( $comics as $index => $comic )
-            <!--SINGLE-CARD-->
-            <li class="comic-book-card">
-                <a href="{{route('comics',$index)}}">
-                    <div class="thumb">
-                        <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
-                    </div>
-                    <h3>{{ $comic['series'] }}</h3>
-                </a>
-            </li>
-            @endforeach
-        </ul>
-    </section>
+    <div class="comics-container">
+        <section id="comics-list" class="small-container card-container">
+            <button class="series">CURRENT SERIES</button>
+            <button class="load">LOAD MORE</button>
+            <ul class="cards-list">
+                @foreach ( $comics as $index => $comic )
+                <!--SINGLE-CARD-->
+                <li class="comic-book-card">
+                    <a href="{{route('comics',$index)}}">
+                        <div class="thumb">
+                            <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                        </div>
+                        <h3>{{ $comic['series'] }}</h3>
+                    </a>
+                </li>
+                @endforeach
+            </ul>
+        </section>
+    </div>
     @include('includes.dc_shop')
 @endsection
 </html>
